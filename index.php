1ea06f0650c605e5313ed8569c20d98097e6dd64
YuriKanegae <yurikanegae@gmail.com>
2020-02-19 16:51:05 -0300

    teste

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset = "UTF-8"/>
+        <title>Menu de projetos</title>
+    </head>
+    <body>
+        <a href = "20200219/Atividade/php/form_agendamento.php">Barbearia</a>
+    </body>
+</html>
