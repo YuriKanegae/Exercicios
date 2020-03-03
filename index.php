505a0982cb4fb46b577790b5fe7530e024a5accd
YuriKanegae <yurikanegae@gmail.com>
2020-03-03 16:56:53 -0300

    20200303'

--- index.php
+++ index.php
@@ -1,11 +1,60 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset = "UTF-8"/>
-        <title>Menu de projetos</title>
+        <meta charset = "UTF-8" />
+        <meta http-equiv = "X-UA-Compatible" content = "IE=edge">
+        <meta name = "viewport" content = "width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css" />
+
+        <title>Portfolio de projetos</title>
     </head>
     <body>
-		<h2>Menu de projetos</h2>
-        <a href = "20200219/Atividade/php/form_agendamento.php">Barbearia</a>
+        <div class = "container">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light" style = "margin-bottom: 30px;">
+                <span class = "navbar-brand mb-0 h1">Yuri Kanegae</span>
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://github.com/YuriKanegae">GIT</a>
+                        </li>
+                        <li class="nav-item">
+                            <p class="nav-link">Prontuário: 1890794</p>
+                        </li>
+						<li class = "nav-item">
+							<a href = "https://arq.ifsp.edu.br/portal/" class = "nav-link">Campos Araraquara</a>
+						</li>
+                    </ul>
+                </div>
+            </nav>
+
+            <table class = "table table striped">
+                <thead class = "thead-dark">
+                    <tr>
+                        <th scope = "col">Nome</th>
+                        <th scope = "col">Data de criação/atualização</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th>
+                            <a href = "20200219/Atividade/php/form_agendamento.php">Barbearia</a>
+                        </th>
+                        <th>
+                            19/02/2020
+                        </th>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <script src = "js/jquery-3.2.1.min.js"></script>
+        <script src = "js/popper.min.js"></script>
+        <script src = "js/bootstrap.min.js"></script>
+        <script src = "js/validaForm.js"></script>
     </body>
 </html>
